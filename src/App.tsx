53140a6629260d9c73f58aec6f845773f43cbc07
Fernando Futila <fernando.futila@gmail.com>
2022-06-26 00:49:41 -0300

    FEAT: Estrutura visual do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,9 @@
-import {useEffect} from "react"
-import {gql, useQuery} from "@apollo/client"
-import { client } from "./lib/apollo"
-
-
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons{
-      id,
-      title
-    }
-  }
-`
-
-
-interface Lesson{
-  id: string,
-  title: string
-}
+import { Event } from "./pages/Event";
 
 function App() {
-
-  //FETCHING DATAS CAN BE DONE LIKE THIS
-  // useEffect(()=> {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY
-  //   }).then(response => {
-  //     console.log(response.data)
-  //   })
-  // }, [])
-
-
-  //OR LIKE THIS
-
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
-
- 
   return (
-    <ul>
-      {data?.lessons.map(lesson => (
-        <li>{lesson.title}</li>
-      ))}
-    </ul>
+    <Event/>
   )
 }
 
-export default App
+export default App;
